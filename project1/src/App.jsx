044244a839707e9d15d18f01221b6438a26f1681
st0rrm <st.0rmwon4416@gmail.com>
2025-10-29 18:14:49 +0900

    배포 수정

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginScreen from './pages/login/login.jsx';
 import HomeScreen from './pages/home/home.jsx';
 import AdminDashboard from './pages/admin/AdminDashboard.jsx';
